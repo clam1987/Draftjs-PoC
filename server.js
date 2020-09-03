c01d18f95c9d18fe1c3a0fa7bdfc54eb1f254e1f
Wilson Lam <lin-1987@live.com>
2020-09-03 16:25:49 -0700

    stuff

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ const apiRoute = require("./routes/apiRoutes");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.use("/api", apiRoute);
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+  
 
 app.listen(PORT, () => console.log(`My server is running on PORT: ${PORT}`));
