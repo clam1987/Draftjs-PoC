a5bfdc69f0fd8e54a868bc9f73714bc6b646f623
Wilson Lam <lin-1987@live.com>
2020-09-03 17:48:27 -0700

    stuff

--- server.js
+++ server.js
@@ -7,16 +7,14 @@ const path = require("path");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'build')));
-
 app.use("/api", apiRoute);
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-      });
-}
+// if (process.env.NODE_ENV === 'production') {
+//     // Serve any static files
+//     app.use(express.static(path.join(__dirname, 'client/build')));
+//     app.get('*', function(req, res) {
+//         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//       });
+// }
 
 app.listen(PORT, () => console.log(`My server is running on PORT: ${PORT}`));
